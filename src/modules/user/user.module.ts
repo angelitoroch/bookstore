d45cbc7d711bcaad8362a7611203dfec82b6c84c
Luis Aboytes <akira_aar@hotmail.es>
2020-02-10 18:35:35 -0600

    Revert "Dev desarrollo"

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
-import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([UserRepository]), SharedModule],
+    imports:[TypeOrmModule.forFeature([UserRepository])],
     providers: [UserService]
 })
 export class UserModule {}
--- src/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MapperService } from './mapper.service';
-
-@Module({
-  exports: [MapperService],
-  providers: [MapperService],
-})
-export class SharedModule {}
